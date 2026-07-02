70256dd0bc3ee2acbf6365fdbd40a1c71f761bce
eze <ezep25728@gmail.com>
2026-07-02 02:21:44 -0300

    feat: implement reusable model evaluation utilities

---
+++ src/models/evaluate.py
@@ -0,0 +1,32 @@
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+def show_metrics(model_name, y_true, y_pred):
+    metrics = {
+        'accuracy': round(accuracy_score(y_true, y_pred), 2),
+        'precision': round(precision_score(y_true, y_pred), 2),
+        'recall': round(recall_score(y_true, y_pred), 2),
+        'f1-score': round(f1_score(y_true, y_pred), 2)
+    }
+    return f'{model_name} metrics: {metrics}'
+
+def show_confusion_matrix(y_true, y_pred):
+    matrix = confusion_matrix(y_true, y_pred)
+    visualizer = ConfusionMatrixDisplay(matrix, display_labels=['Sin anemia', 'Con anemia'])
+    return visualizer.plot()
+
+def show_cv_metrics(model_scores):
+    metrics = {
+        "accuracy":
+            model_scores["test_accuracy"].mean().round(4),
+
+        "precision":
+            model_scores["test_precision"].mean().round(4),
+
+        "recall":
+            model_scores["test_recall"].mean().round(4),
+
+        "f1":
+            model_scores["test_f1"].mean().round(4)
+            }
+    return metrics
