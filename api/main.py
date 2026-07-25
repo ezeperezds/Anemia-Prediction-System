01f7138e615642a05b05e6e1adf5df367b131025
eze <ezep25728@gmail.com>
2026-07-25 01:24:22 -0300

    feat: configure API metadata

--- api/main.py
+++ api/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+description = """
+This API exposes a trained machine learning pipeline capable of predicting
+anemia from patient hematological measurements.
+
+The inference pipeline automatically performs feature engineering,
+preprocessing, feature selection, and classification before returning
+the prediction.
+"""
+
+app = FastAPI(
+    title = 'Anemia Prediction API',
+    description = description,
+    summary = 'Machine Learning API for anemia prediction.',
+    version = '1.0',
+    contact = {
+        'name': 'Ezequiel Alejandro Pérez',
+        'email': 'ezep5993@gmail.com',
+        'url': 'https://github.com/ezeperezds'
+        }
+)
 
 @app.get("/")
 async def root():
